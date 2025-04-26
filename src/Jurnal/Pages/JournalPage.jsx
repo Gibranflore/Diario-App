ea51da2c598255728830fd5129537a8037e50f7e
Gibran Flores Mendieta <gibran.floresmendieta@gmail.com>
2025-04-26 16:56:55 -0600

    agregar galeria de imagenes, boton

--- src/Jurnal/Pages/JournalPage.jsx
+++ src/Jurnal/Pages/JournalPage.jsx
@@ -1,10 +1,27 @@
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../Layout/JournalLayout'
 import { NothingSelected, NotView } from '../View/index'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NotView/>
+      <NothingSelected/>
+
+        <IconButton
+          size='large'
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', 'opacity': 0.7},
+            position: 'fixed',
+            right: 50,
+            bottom: 50,
+          }}
+        >
+          <AddOutlined sx={{fontSize: 30}}/>
+        </IconButton>
+
     </JournalLayout>
   )
 }
--- src/Jurnal/View/NotView.jsx
+++ src/Jurnal/View/NotView.jsx
@@ -5,40 +5,41 @@ import { ImageGallery } from '../components'
 
 export const NotView = () => {
     return (
-        <Grid container direction='column' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
-        <Grid item>
-            <Typography fontSize={ 39 } fontWeight='light' >Hola desde la vista</Typography>
+        <Grid container direction='column' justifyContent='space-between' alignItems='center' sx={{width: '100%', mb: 1 }}>
+            <Grid item>
+                <Typography fontSize={ 39 } fontWeight='light' >Hola desde la vista</Typography>
+            </Grid>
+
+            <Grid item>
+                <Button color="primary" sx={{ padding: 2 }}>
+                    <SaveAltOutlined sx={{ fontSize: 30, mr: 1 }} />
+                    Guardar
+                </Button>
+            </Grid>
+
+            <Grid container>
+                <TextField 
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    placeholder="Ingrese un título"
+                    label="Título"
+                    sx={{ border: 'none', mb: 1 }}
+                />
+
+                <TextField 
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    placeholder="¿Qué sucedió en el día de hoy?"
+                    minRows={ 5 }
+                />
+            </Grid>
+
+            {/* Image gallery */}
+            <ImageGallery />
+
         </Grid>
-        <Grid item>
-            <Button color="primary" sx={{ padding: 2 }}>
-                <SaveAltOutlined sx={{ fontSize: 30, mr: 1 }} />
-                Guardar
-            </Button>
-        </Grid>
-
-        <Grid container>
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                placeholder="Ingrese un título"
-                label="Título"
-                sx={{ border: 'none', mb: 1 }}
-            />
-
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                multiline
-                placeholder="¿Qué sucedió en el día de hoy?"
-                minRows={ 5 }
-            />
-        </Grid>
-
-        {/* Image gallery */}
-        <ImageGallery />
-
-    </Grid>
     )
 }
